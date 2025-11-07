4114b72bb3d7216dac7540884f5b496971b3df70
NicXiee95 <alexienicole13@gmail.com>
2025-11-07 23:13:50 +0800

    answers.txt
    1. changed the player  of x and y  into my first and second name
    2. changed the treasure_e with the last letter of my last name
    3. Change the following if-move letters w, a, s, d to up/down/left/right

--- romarate.py
+++ romarate.py
@@ -6,25 +6,26 @@ treasure_y = 3
 game_running = True
 
 print(f"Find the treasure at ({treasure_x}, {treasure_y})!")
+
 while game_running:
   move = input("Enter move (up/down/left/right): ")
-# TODO: update player_x and player_y based on move
-print(f"Player position: ({player_x}, {player_y})")
-# TODO: check if player has reached the treasure
-move = input("Enter move (up/down/left/right): ")
-if move == "w":
-  player_y += 1
-if move == "s":
-  player_y -= 1
-if move == "a":
-  player_x -= 1
-if move == "d":
-  player_x += 1
+
+if move == "up":
+  player_nicole += 1
+if move == "down":
+  player_nicole -= 1
+if move == "left":
+  player_alexie -= 1
+if move == "right":
+  player_alexie += 1
 if move == "q":
-  game_running = False
+  print("Maybe next time=(")
+  break
 else:
   print("invalid move!)
-print(f"player position: ({player_x}, {player_y})")
+        
+print(f"player position: ({player_alexie}, {player_nicole})")
+        
 if player_x == treasure_x and player_y == treasure_y:
   print("You Found the treasure!")
   game_running = False
